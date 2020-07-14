dd4cb05029d2f0e596884b4a6b7907c7b7aac9f4
Paul DelPadre <PDelPadre@rice.local>
2020-07-14 15:41:07 -0400

    updating api routes post works now

--- routes/routesAPI.js
+++ routes/routesAPI.js
@@ -1,6 +1,8 @@
 const db = require ("../db/db.json");
 const fs = require ("fs");
 const path = require ("path");
+const { v1: uuidv1 } = require("uuid");
+
 
 module.exports = function (app) {
     app.get("/api/notes", (req, res) => {
@@ -9,33 +11,23 @@ module.exports = function (app) {
             // console.log("should be data",JSON.parse(data));
             res.json(db);
     });
-    // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-    // app.post("/api/notes", (req, res) => {
-//     const noteObj = req.body;
+
+   // * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
     app.post("/api/notes", (req, res) => {
-        let dataArray = db;
-/////////// recode here//////////////
-            const userNote = JSON.parse(data);
-            console.log(userNote);
+            let dataArray = db;
+            console.log(dataArray);
             const userNewNote = req.body;
             console.log(userNewNote);
-            userNote.push(userNewNote);
-
-            fs.writeFileSync("../db/db.json", JSON.stringify(userNote), "utf8");
-            res.json(userNote);
-            // end recode/////////////////
+            userNewNote.id = uuidv1();
+            console.log(userNewNote.id);
+            dataArray.push(userNewNote);
+            fs.writeFileSync("db/db.json", JSON.stringify(dataArray), "utf8");
+            res.json(dataArray);
     });
 
 }
-// * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
-
-
-
-// })
-
-
-
+//    This section will delete the notes by the note id
 // app.delete("/api/notes/:id", (req, res) => {
 
 

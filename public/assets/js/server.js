5c259ceb048e4009634a094cd2c6a21c478f15ec
Paul DelPadre <PDelPadre@rice.local>
2020-07-13 23:48:06 -0400

    working on api routes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -13,36 +13,14 @@ app.use(express.static("public"));
 ////////////// Router///////////////////
 require("../routes/routesAPI")(app);
 require("../routes/routesHTML")(app);
-///////////////// Routes//////////////////////
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../../index.html"));
-// });
-// app.get("/notes", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../../notes.html"));
-// });
-// app.get("*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../../index.html"));
-// });
-
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../../public/index.html"));
-// });
-
-//////////////// End of Routes///////////////
-
-//////////////// API Routes///////////////
 // app.get("/api/characters/:character", function(req, res) {
 
 
 
-//////////////// End of API Routes///////////
-app.post("/api/notes", (req, res) => {
-    const noteObj = req.body;
-    // Example oof how to add an object to the body
-    // noteObj.id = 3;
+// app.post("/api/notes", (req, res) => {
+//     const noteObj = req.body;
 
-
-})
+// })
 
 
 app.listen(PORT, () => {
@@ -50,7 +28,7 @@ app.listen(PORT, () => {
 
 })
 
-app.delete("/api/notes/:id", (req, res) => {
+// app.delete("/api/notes/:id", (req, res) => {
 
 
-})
+// })
--- public/assets/routes/routesAPI.js
+++ public/assets/routes/routesAPI.js
@@ -3,12 +3,38 @@ const fs = require ("fs");
 const path = require ("path");
 
 module.exports = function (app) {
+    app.get("/api/notes", (req, res) => {
+        fs.readFile("../../../db/db.json", "utf-8", (err, data) => {
+            res.json(JSON.parse(data));
+        })
+    });
     // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+    // app.post("/api/notes", (req, res) => {
+//     const noteObj = req.body;
+    app.post("/api/notes", (req, res) => {
+        fs.readFile("../../../db/db.json", "utf-8";
 
-    app.get("/api/notes", function(req, res){
-        res.sendFile(path.join(__dirname, "../../../db/db.json"))
-        });
-    
+            const userNote = JSON.parse(data);
+            const userNewNote = req.body;
+
+            userNote.push(userNewNote);
+
+            fs.writeFileSync("../../../db/db.json", JSON.stringify(userNote), "utf8");
+            res.json(userNote);
+        );
+    });
+
 }
-
 // * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+
+
+
+// })
+
+
+
+// app.delete("/api/notes/:id", (req, res) => {
+
+
+// })
